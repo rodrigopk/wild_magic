25ed23c257d58d1a8d6b99a82c981e28b6c00a5d
rodrigopk <rodrigopk@gmail.com>
2019-03-02 20:07:24 +0100

    Extract effects yaml file path to constant

--- lib/magic/effects.rb
+++ lib/magic/effects.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+require_relative './dice'
+
 module Magic
   class Effects
+    MAGICAL_EFFECTS_YAML_FILE = 'config/nlrme.yml'
+
     def initialize(dependencies = {})
       @dice = dependencies.fetch(:dice) do
-        Dice.new
+        Magic::Dice.new
       end
 
-      @effects = YAML.load_file('config/nlrme.yml')
+      @effect_list = YAML.load_file(MAGICAL_EFFECTS_YAML_FILE)
     end
 
     def pick_random_effect
-      @effects['effects'][@dice.roll]
+      @effect_list['effects'][@dice.roll]
     end
   end
 end
