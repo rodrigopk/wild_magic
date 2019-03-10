6c210129ef647b08f6fb0e61004788c53e52bf69
rodrigopk <rodrigopk@gmail.com>
2019-03-10 08:39:55 +0100

    B[GSJu0HKu]: Fix failing test for HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post '/', to: 'home#roll', as: 'roll'
 
   # get '/patients/:id', to: 'patients#show', as: 'patient'
+  get '/', to: 'home#index', as: 'home_index'
   root 'home#index'
 end
