71500ac8e8148aba32b098dc2ccfa7100a260b71
rodrigopk <rodrigopk@gmail.com>
2019-03-10 08:26:45 +0100

    B[BKb7RMlY]: Fix missing entries in yaml file   The yaml file containing the magical effects list was missing about 102 entries.   Added the missing entries and added test to check for that.

--- test/magic/effects_test.rb
+++ test/magic/effects_test.rb
@@ -18,5 +18,12 @@ module Magic
       effects = Magic::Effects.new(dice: mock_dice)
       assert_equal('Caster is chained to an anvil', effects.pick_random_effect)
     end
+
+    test 'has 10_000 magical effects' do
+      effects = Magic::Effects.new
+
+      magical_effects = effects.magical_effects
+      assert_equal(10_000, magical_effects.count)
+    end
   end
 end
